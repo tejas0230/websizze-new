165eff816c3651e869a5d3e48ec1d6d85eabbb7e
Tejas . <tejas0215lc@gmail.com>
2025-01-30 05:03:59 +0530

    added lenis scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,21 @@
 "use client";
 import type { Metadata } from "next";
 import { Zain, Poppins, Whisper } from "next/font/google";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import "locomotive-scroll/dist/locomotive-scroll.css";
 import "./globals.css";
 import Header from "./header";
 import Footer from "./footer";
+import Lenis from 'lenis';
 
 const zain = Zain({
   subsets: ["latin"],
-  weight: ["300", "400", "700"],
+  weight: ["300","400","700"],
 });
 
 const poppins = Poppins({
   subsets: ["latin"],
-  weight: ["300", "400", "700"],
+  weight: ["300","400", "700"],
 });
 
 const whisper = Whisper({
@@ -32,49 +33,19 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const scrollContainerRef = useRef<HTMLDivElement>(null);
-  const [locomotiveScroll, setLocomotiveScroll] = useState<any>(null);
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const isMobile = window.innerWidth <= 768; // Check if it's a mobile device
-      if (!isMobile && scrollContainerRef.current) {
-        import("locomotive-scroll").then((LocomotiveScroll) => {
-          const scrollInstance = new LocomotiveScroll.default({
-            el: scrollContainerRef.current as HTMLElement,
-            smooth: true,
-            smartphone: {
-              smooth: true, // Enable smooth scroll on mobile only if you want
-            },
-            tablet: {
-              smooth: true,
-              breakpoint: 1024, // Set breakpoint for tablet devices
-            },
-            getDirection: true,
-            multiplier: 1,
-            lerp: 0.1,
-          });
-
-          setLocomotiveScroll(scrollInstance);
-
-          return () => {
-            scrollInstance.destroy();
-          };
-        });
-      }
+  useEffect(()=>{
+    const lenis = new Lenis();
+    function raf(time:any){
+      lenis.raf(time);
+      requestAnimationFrame(raf)
     }
-  }, []);
-
+    requestAnimationFrame(raf)
+  },[]);
   return (
     <html lang="en">
-      <body
-        className={`${poppins.className} ${zain.className} ${whisper.className} antialiased main-scrollbar`}
-      >
-        <div
-          ref={scrollContainerRef}
-          data-scroll-container
-          style={typeof window !== "undefined" && window.innerWidth <= 768 ? { overflowY: "auto" } : {}}
-        >
+      <body className={`${zain.className} ${poppins.className} ${whisper.className}antialiased main-scrollbar`}>
+        <div>
           <Header />
           <div>{children}</div>
           <Footer />
