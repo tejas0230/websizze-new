6bbf4155024022804cc391634227de46b5c894da
Tejas . <tejas0215lc@gmail.com>
2025-01-29 16:55:57 +0530

    es lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,21 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react/jsx-no-undef": "off", // Disable errors for undefined JSX components
+      "no-unused-vars": [
+        "warn",
+        {
+          varsIgnorePattern: "^_", // Ignore variables prefixed with _
+          argsIgnorePattern: "^_", // Ignore function arguments prefixed with _
+          destructuredArrayIgnorePattern: "^_", // Ignore destructured variables prefixed with _
+        },
+      ],
+      "react/no-unused-prop-types": "off", // Prevent errors for unused props
+      "react/no-unused-state": "off", // Prevent errors for unused state
+    },
+  },
 ];
 
 export default eslintConfig;
