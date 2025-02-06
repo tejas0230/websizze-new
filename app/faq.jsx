202348eb52bf600d66b777cebd03b24a1fdc3549
Tejas . <tejas0215lc@gmail.com>
2025-02-06 21:52:01 +0530

    Added 0$ pricing card

--- app/faq.jsx
+++ app/faq.jsx
@@ -31,19 +31,19 @@ export default function FAQ() {
             <motion.div initial={{opacity:0, y:10}} whileInView={{ opacity: 1,y:0 }} viewport={{ once: true }}
         transition={{ ease: "easeOut", duration: 1 }} className="m-auto w-full lg:w-1/2">
             <Accordion type="single" collapsible className="font-[poppins]  m-auto">
-                <AccordionItem value="item-1" className="border-[0.1px] border-[#969696] rounded-xl px-5 m-4 hero-bg-3">
+                <AccordionItem value="item-1" className="border-[0.1px] border-[#969696] rounded-xl px-5 m-4 hero-bg-5">
                     <AccordionTrigger><div className="text-base font-light">Who’s behind Websizze?</div></AccordionTrigger>
                     <AccordionContent>
                     <div className="text-base font-light text-[#D0D0D0]">Websizze is led by a team of passionate and experienced web developers, designers, and strategists dedicated to building high-quality websites that drive results.</div>
                     </AccordionContent>
                 </AccordionItem>
-                <AccordionItem value="item-2" className="border-[0.1px] border-[#969696] rounded-xl px-5 m-4 hero-bg-3">
+                <AccordionItem value="item-2" className="border-[0.1px] border-[#969696] rounded-xl px-5 m-4 hero-bg-5">
                     <AccordionTrigger><div className="text-base font-light">Can I get a refund?</div></AccordionTrigger>
                     <AccordionContent>
                     <div className="text-base font-light text-[#D0D0D0]">We strive to deliver exceptional results, and client satisfaction is our priority. If you're not satisfied with our work, we offer refunds based on the terms outlined in our refund policy.</div>
                     </AccordionContent>
                 </AccordionItem>
-                <AccordionItem value="item-3" className="border-[0.1px] border-[#969696] rounded-xl px-5 m-4 hero-bg-3">
+                <AccordionItem value="item-3" className="border-[0.1px] border-[#969696] rounded-xl px-5 m-4 hero-bg-5">
                     <AccordionTrigger><div className="text-base font-light">How quickly will I receive my project?</div></AccordionTrigger>
                     <AccordionContent>
                     <div className="text-base font-light text-[#D0D0D0]">Project timelines vary depending on complexity and requirements. A standard website typically takes between 2 to 6 weeks from design to launch. For urgent projects, we offer expedited development options. Get in touch, and we’ll provide a tailored timeline based on your needs.</div>
